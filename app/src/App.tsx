9ccf0ce1f2883d108a5d430a366f36abde7e1af1
PC <kim000183@naver.com>
2023-02-18 14:07:11 +0900

    파일제거

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,6 +1,5 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
-import './App.css';
 import styled from "styled-components";
 import ContentBox from './component/Contents';
 
